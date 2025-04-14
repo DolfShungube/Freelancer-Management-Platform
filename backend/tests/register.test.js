a28fa6f0d149342b6a1f06edecd6f6074b25fc41
DolfShungube <dolfshungube@gmail.com>
2025-04-14 04:00:42 +0200

    updated the register tests+ added workflow

--- backend/tests/register.test.js
+++ backend/tests/register.test.js
@@ -1,39 +1,55 @@
-const { validateInputs, registerUser } = require('../register/register_functions.js');
+function registrationDetailsPresent(details){
+
+    if(Object.values(details).includes('')){
+       return false
+    }
+
+return true
+
+}
+
+function existingEmail(email){
+
+if(email===''){
+    return true;
+    
+}
+
+return false;
+
+}
+
+/////
 
 
-describe("Registration Logic",()=>{
-    let usersMap;
+test("checking if we recived an existing email from the server",()=>{
+    const result = existingEmail("dolphnoble@gmail.com");
+    expect(result).toBe(false);
+});
 
-    beforeEach(()=>{
-        usersMap = new Map();
-    });
+test("email does not exist in database",()=>{
+    const result = existingEmail("");
+    expect(result).toBe(true);
+});
 
-    test("Should return error if the password do not match",()=>{
-        const result = validateInputs("Dolph","Bhekiswayo","dolphnoble@gmail.com","password134","password1234","Freelancer");
-        expect(result).toBe("Passwords do not match! Please re-check");
-    });
 
-    test("Should return error if there is a missing information",()=>{
-        const result = validateInputs("Dolph","","dolphnoble@gmail.com","password1234","","Freelancer");
-        expect(result).toBe("Ensure you provide all necessary information.");
-    });
 
-    test("Should return error if user already exists",()=>{
-        usersMap.set("dolphnoble@gmail.com",{firstname:"Dolph",lastname:"Bhekiswayo",password:"password1234",accounttype:"freelancer"})
-        const result = registerUser(usersMap,"Dolph","Bhekiswayo","dolphnoble@gmail.com","password1234","password1234","Freelancer");
-        expect(result).toBe("User already registered with this email!");
-    });
+test("if all  details  not present",()=>{
+    const result = registrationDetailsPresent({userType:"",email:"",password:""});
+    expect(result).toBe(false);
+});
 
-    test("Should succefully register a new user",()=>{
-        const result = registerUser(usersMap, "John", "Doe", "john.doe@example.com", "password123", "freelancer");
-        expect(result).toBe("User Registered");
-        expect(usersMap.size).toBe(1);
-        expect(usersMap.get("john.doe@example.com")).toEqual({ firstname: "John", lastname: "Doe", password: "password123", accountType: "freelancer" });
 
-    });
+test("if all  details  not present",()=>{
+    const result = registrationDetailsPresent({userType:"user",email:"",password:"password"});
+    expect(result).toBe(false);
+});
 
-    test("All necessary details  are provided", () => {
-        const validationMessage = validateInputs("John", "Doe", "john.doe@example.com", "password123", "password123", "Freelancer");
-        expect(validationMessage).toBeNull(); 
-    });
+
+
+
+
+test("if user details  present",()=>{
+    const result = registrationDetailsPresent({userType:"user",email:"email",password:"password"});
+    expect(result).toBe(true);
 });
