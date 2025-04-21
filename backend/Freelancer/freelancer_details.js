08ca49b86b9253445408155345d21af3c1d72cef
Bokang Madondo <bokangmadondom@gmail.com>
2025-04-22 00:57:27 +0200

    Fixed the Freelancer Profile redundancy

--- backend/Freelancer/freelancer_details.js
+++ backend/Freelancer/freelancer_details.js
@@ -2,10 +2,12 @@ import supabase from '../config/superbaseClient.js';
 
 const fetchinfo = async () => {
   const Freelancer_info = document.querySelector('.profile-info');
-
+  const {data:{user},error:authError} = await supabase.auth.getUser();
   const { data, error } = await supabase
     .from('Freelancer')
-    .select();
+    .select("*")
+    .eq("id", user.id)
+    .single()
 
   if (error) {
     Freelancer_info.innerHTML = '<p>Could not find profile.</p>';
@@ -26,7 +28,7 @@ const fetchinfo = async () => {
     FreelancerProfile.classList.add('profile-text');
 
     FreelancerProfile.innerHTML = `
-    <img src="images/profile_picture.jpg" alt="Profile Picture" class="profile-pic" />
+    <img src="${freelancer.profilepicture}" alt="Profile Picture" class="profile-pic" />
       <p class="greeting">Hi, ${freelancer.firstname} ${freelancer.lastname}</p>
       <p class="role">Freelancer</p>
     `;
