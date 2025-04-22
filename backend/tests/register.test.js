9572f6a7700d72629ab4708d3e5e8cff57906093
NobleWolf <dolfshungube@gmail.com>
2025-04-22 07:51:05 +0200

    fixed typo in register.test.js

--- backend/tests/register.test.js
+++ backend/tests/register.test.js
@@ -77,30 +77,30 @@ function jobs(job){
 
 test("if a user posts a job then they should be able to get details about it",()=>{
     const result = getJob("id1");
-    expect(results).toEqual({decsription:"description",amount:"100",name:"name"})
+    expect(result).toEqual({decsription:"description",amount:"100",name:"name"})
     
 })
     test("if a user has not posted a job then they should  not have details for any job",()=>{
     const result = getJob("id2");
-    expect(results).toEqual("you have not posted any jobs")
+    expect(result).toEqual("you have not posted any jobs")
     
 })
 
 test("if a job is available a freelancer should be able to accept it",()=>{
     const result = jobs({status:"free",description:"description"});
-    expect(results).toEqual("accepted")
+    expect(result).toEqual("accepted")
     
 })
 
 test("if a job is not available a freelancer should not be able to accept it",()=>{
     const result = jobs({status:"free",description:"description"});
-    expect(results).toEqual("taken")
+    expect(result).toEqual("taken")
     
 })
 
 test("if an admin can see all users",()=>{
     const result = viewUsers();
-    expect(results).toEqual(["user1","user2"])
+    expect(result).toEqual(["user1","user2"])
     
 })
 
