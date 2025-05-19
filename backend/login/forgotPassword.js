00f9010d6aec70026237b75533b1ee37f42f1a39
NobleWolf <dolfshungube@gmail.com>
2025-05-19 08:29:44 +0200

    Updated change password feature

--- backend/login/forgotPassword.js
+++ backend/login/forgotPassword.js
@@ -4,23 +4,27 @@ import supabase from "../config/superbaseClient.js"
 
 const form= document.getElementById('form')
 
-
 form.addEventListener('submit',async (e)=>{
      e.preventDefault()   
     const email= document.getElementById("email").value
     localStorage.setItem("resetEemail", email);
 
 
- 
-  
-
         try {
-            let { data, error } = await supabase.auth.resetPasswordForEmail(email);
+            let { data, error } = await supabase.auth.resetPasswordForEmail(email,{
+                redirectTo: `https://victorious-tree-0efc89c00.6.azurestaticapps.net/frontend/src/pages/newPass.html`
+            });
         if(error){
             showAlert(error.message,"error")   
 
         }else{
             showAlert("check your emails for reset link",'success') 
+
+        setTimeout(() => {
+             window.location.replace('./login.html')
+        }, 2000);
+
+            
 
         }
 
@@ -30,14 +34,12 @@ form.addEventListener('submit',async (e)=>{
             
         }
 
-     window.location.href = `newPass.html?email=${encodeURIComponent(email)}`;
 
-    window.location.replace('./login.html')
+
 
 
 
 })
-
 
 
 
